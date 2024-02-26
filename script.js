04d9246632b4a9a89f74122c853a4761e8b84c1d
aishwaryasupekar7890 <158082389+aishwaryasupekar7890@users.noreply.github.com>
2024-02-26 13:06:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+  function contactus()
+  {
+    window.location.replace("http://127.0.0.1:5500/contact.html");
+   document.getElementById("btn").onclick = contactus;
+  }
+  
+
+  function myFunction() 
+  {
+    alert("Send Succesfullly!");
+    document.getElementById("btnn").onclick = myFunction;
+   }
+
+
+   function myprice() 
+   {
+     alert("For More Details Visit To Our Near By Office");
+     document.getElementById("btnn").onclick = myFunction;
+    }
+ 
